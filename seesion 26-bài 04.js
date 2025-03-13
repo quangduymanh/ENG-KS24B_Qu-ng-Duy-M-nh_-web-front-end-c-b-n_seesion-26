54cc93e8221d9b24af2037ac7bbff0675b147b4c
quangduymanh <quangduymanh4@gmail.com>
2025-03-13 08:47:27 +0700

    upload bài 04

---
+++ seesion 26-bài 04.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5,6,7];
+if (!Array.isArray(arr)) {
+    console.log("Dữ liệu không hợp lệ");
+}else if (arr.length===0){
+    console.log("mảng không chứa phần tử nào")
+}else{
+    let number = arr.filter
+}
